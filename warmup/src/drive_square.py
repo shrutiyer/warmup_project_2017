2336c30c6568e9c4fe5bf4de122111604775813a
hdavidzhu <hdavidzhu@gmail.com>
2017-01-26 13:09:07 -0500

    UPDATE.

--- warmup/src/drive_square.py
+++ warmup/src/drive_square.py
@@ -4,16 +4,19 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, Vector3
 import tf
 import rospy
+import math
 
 rospy.init_node('drive_square')
 
 def on_odom_received(odom):
+    position = odom.pose.pose.position
     yaw = tf.transformations.euler_from_quaternion((
         odom.pose.pose.orientation.x,
         odom.pose.pose.orientation.y,
         odom.pose.pose.orientation.z,
-        odom.pose.pose.orientation.w))[2]
-    print yaw
+        odom.pose.pose.orientation.w))[2] * 180 / (math.pi)
+
+
 
 listener = rospy.Subscriber('/odom', Odometry, on_odom_received)
 
