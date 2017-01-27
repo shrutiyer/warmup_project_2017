58da3eed841dc72e9c90bb016b0c74918485928d
hdavidzhu <hdavidzhu@gmail.com>
2017-01-27 15:59:57 -0500

    REMOVE: Prints and markers.

--- warmup/src/drive_square.py
+++ warmup/src/drive_square.py
@@ -10,9 +10,7 @@ import tf
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, Vector3
-from visualization_msgs.msg import Marker
 
-from marker import MarkerMaker # TODO: Deprecate.
 from models.position import Position
 
 # CLASSES ======================================================================
@@ -35,22 +33,17 @@ class SquareDrivingController(object):
         self.theta_pos = 0.2
         self.theta_angle = 8 # In degrees
 
-        # TODO: Deprecate.
-        self.is_callbacked = False
-
         rospy.init_node('drive_square')
 
         self.odom_listener = rospy.Subscriber('odom', Odometry, self.on_odom_received)
         self.twist_publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        self.has_read_data_once = False
 
         self.twist_forward = Twist()
         self.twist_forward.linear.x = 1; self.twist_forward.linear.y = 0; self.twist_forward.linear.z = 0
         self.twist_left = Twist()
         self.twist_left.angular.z = 0.2
         self.twist_stop = Twist()
-
-        # TODO: Deprecate
-        self.marker_maker = MarkerMaker()
 
     def on_odom_received(self, odom):
         self.curr_pos.x = odom.pose.pose.position.x
@@ -60,9 +53,7 @@ class SquareDrivingController(object):
             odom.pose.pose.orientation.y,
             odom.pose.pose.orientation.z,
             odom.pose.pose.orientation.w))[2] * 180 / (math.pi)
-
-        # TODO: Deprecate.
-        self.is_callbacked = True
+        self.has_read_data_once = True
 
     def is_in_position(self):
         error = math.sqrt((self.target_pos.x - self.curr_pos.x)**2
@@ -81,25 +72,21 @@ class SquareDrivingController(object):
         self.target_angle = ((self.curr_angle + 180 + 90) % 360) - 180
 
     def run(self):
-        print "running"
         r = rospy.Rate(1000)
         while not rospy.is_shutdown():
-            if not self.is_callbacked:
+
+            # Prevents the state machine from running when no data has yet been received.
+            if not self.has_read_data_once:
                 r.sleep()
                 continue
 
             # State machine.
             if self.current_state == States.CALC_FORWARD:
-                print "calculating"
                 self.set_target_pos()
-                self.marker_maker.set_position(self.target_pos)
-                self.marker_maker.publish()
-
-                self.current_state = States.MOVE_FORWARD
                 self.twist_publisher.publish(self.twist_forward)
+                self.current_state = States.MOVE_FORWARD
 
             elif self.current_state == States.MOVE_FORWARD:
-                print "moving forward"
                 if self.is_in_position():
                     self.current_state = States.CALC_TURN
                     self.twist_publisher.publish(self.twist_stop)
@@ -116,10 +103,6 @@ class SquareDrivingController(object):
 
             elif current_state == States.STOP:
                 self.twist_publisher.publish(self.twist_stop)
-                print "Stopped."
-
-            else:
-                print "Done."
 
             r.sleep()
 
