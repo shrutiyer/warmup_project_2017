d6c4eb466a6d0df2fa26a96b5d3d7c9666571f37
YuzhongHuang <yuzhong.huang@students.olin.edu>
2017-01-27 14:37:12 -0500

    teleop program, haven't tested on robot

--- warmup/src/teleop.py
+++ warmup/src/teleop.py
@@ -1,7 +1,23 @@
-import tty
-import select
-import sys
-import termios
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+import sys, select, termios, tty
+
+actions = {
+    'w':(1,0,0,0),
+    'a':(0,0,0,1),
+    's':(-1,0,0,0),
+    'd':(0,0,0,-1),
+    'k':(0,0,0,0)
+}
+
+prints = {
+    'w':'moving forward',
+    'a':'turning left',
+    's':'moving backward',
+    'd':'turning right',
+    'k':'stopped'
+}
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -13,6 +29,33 @@ def getKey():
 settings = termios.tcgetattr(sys.stdin)
 key = None
 
+publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+rospy.init_node('teleop')
+
 while key != '\x03':
     key = getKey()
-    print key
+
+    x = 0
+    y = 0
+    z = 0
+    w = 0
+
+    if key in actions:
+        print prints[key]
+
+        x = actions[key][0]
+        y = actions[key][1]
+        z = actions[key][2]
+        w = actions[key][3]
+
+        twist = Twist()
+        twist.linear.x = x
+        twist.linear.y = y 
+        twist.linear.z = z
+        twist.angular.z = w
+
+        publisher.publish(twist)
+
+    else:
+        print "Not a valid action"
+
