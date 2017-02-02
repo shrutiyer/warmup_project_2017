b26393f075afc491c535d172c19ea9e2b56f78dd
hdavidzhu <hdavidzhu@gmail.com>
2017-02-02 16:46:27 -0500

    FIX: Syntax errors in obstacle avoidance.

--- warmup/src/obstacle_avoidance.py
+++ warmup/src/obstacle_avoidance.py
@@ -17,7 +17,7 @@ class ObstacleAvoiding(object):
         self.front_angle_range = 90
         self.distance_range = (0.4, 3)  # Tuple of (min, max)
         self.odom_received = False
-        
+
         self.goal_odom = (1, 0)   # Tuple of goal's (x, y) in odom
         self.goal_bl = (0, 0)     # Goal in base_link
         self.curr_pos_odom = (0, 0)
@@ -27,6 +27,7 @@ class ObstacleAvoiding(object):
         rospy.init_node('obstacle_avoidance')
         self.odom_listener = rospy.Subscriber('odom', Odometry, self.on_odom_received)
         self.laser_listener = rospy.Subscriber('/stable_scan', LaserScan, self.on_laser_received)
+        self.twist_publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 5)
 
         self.twist = Twist()
 
@@ -45,23 +46,26 @@ class ObstacleAvoiding(object):
         self.twist.angular.z = attractive_twist[1]
 
     def attractive_forces(self):
-        goal_distance = (self.goal_odom[0]-self.curr_pos_odom[0])**2
-                        +(self.goal_odom[0]-self.curr_pos_odom[0])**2
-        self.goal_bl = self.odom_to_base_link(self.goal_odom)
-        goal_angle = math.atan(self.goal_bl[1]/float(self.goal_bl[0]))
-        angular = goal_angle/(2*math.pi)
+        goal_distance = math.sqrt(
+            (self.goal_odom[0] - self.curr_pos_odom[0])**2 +
+            (self.goal_odom[0] - self.curr_pos_odom[0])**2)
 
-        if goal_radius<=goal_distance:
+        self.goal_bl = self.odom_to_base_link(self.goal_odom)
+        goal_angle = math.atan2(self.goal_bl[1], float(self.goal_bl[0]))
+        angular = goal_angle / (2*math.pi)
+
+        if self.goal_radius <= goal_distance:
             linear = 1
-        elif goal_distance<goal_radius
-            linear = goal_distance/goal_radius
+        elif goal_distance < self.goal_radius:
+            linear = goal_distance / self.goal_radius
 
         return (linear,angular)
 
     def odom_to_base_link(self, odom_point):
         # Receives a tuple of odom
-        return (odom_point[0]*math.cos(self.curr_angle) + odom_point[1]*math.sin(self.curr_angle),
-                -odom_point[0]*math.sin(self.curr_angle) + odom_point[1]*math.cos(self.curr_angle))
+        return (
+            odom_point[0] * math.cos(self.curr_angle) + odom_point[1] * math.sin(self.curr_angle),
+            -1 * odom_point[0] * math.sin(self.curr_angle) + odom_point[1] * math.cos(self.curr_angle))
 
     def base_link_to_odom(self):
         pass
@@ -70,18 +74,17 @@ class ObstacleAvoiding(object):
         curr_pos_odom = (odom.pose.pose.position.x[0],odom.pose.pose.position.y[1])
         if not self.odom_received:
             # Set the goal distance in odom
-            self.goal = (odom.pose.pose.position.x[0]+self.goal_input[0], 
-                            odom.pose.pose.position.y[1]+self.goal_input[1]) 
+            self.goal = (odom.pose.pose.position.x[0]+self.goal_input[0],
+                odom.pose.pose.position.y[1]+self.goal_input[1])
             self.curr_angle = tf.transformations.euler_from_quaternion((
-            odom.pose.pose.orientation.x,
-            odom.pose.pose.orientation.y,
-            odom.pose.pose.orientation.z,
-            odom.pose.pose.orientation.w))[2]
+                odom.pose.pose.orientation.x,
+                odom.pose.pose.orientation.y,
+                odom.pose.pose.orientation.z,
+                odom.pose.pose.orientation.w))[2]
             self.odom_received = True
 
     def on_laser_received(self, laser_array):
         pass
-
 
 # EXECUTE ======================================================================
 
