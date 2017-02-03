49d4fce8194c4339c62e422be418df45e036edb5
hdavidzhu <hdavidzhu@gmail.com>
2017-02-03 17:08:50 -0500

    ADD: Simple state machine with Smach.

---
+++ warmup/src/my_fsm.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+import smach_ros
+
+class StateGetToGoal(smach.State):
+
+    class Outcomes:
+        goal_achieved = 'goal_achieved'
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes = [self.Outcomes.goal_achieved])
+
+    def execute(self, userdata):
+        goal_achieved = True
+        if goal_achieved: # TODO: Define this logic.
+            return self.Outcomes.goal_achieved
+
+
+class StateDriveSquare(smach.State):
+
+    class Outcomes:
+        square_made = 'square_made'
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes = [self.Outcomes.square_made])
+
+    def execute(self, userdata):
+        square_made = True
+        if square_made: # TODO: Define this logic
+            return self.Outcomes.square_made
+
+def main():
+    rospy.init_node('my_fsm')
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes =  ['finished'])
+
+    with sm:
+        smach.StateMachine.add('GET_TO_GOAL', StateGetToGoal(), transitions = {
+            StateGetToGoal.Outcomes.goal_achieved: 'DRIVE_SQUARE'
+        })
+        smach.StateMachine.add('DRIVE_SQUARE', StateDriveSquare(), transitions = {
+            StateDriveSquare.Outcomes.square_made: 'finished'
+        })
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+
+if __name__ == '__main__':
+    main()
